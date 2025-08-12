a0c4cee6b644c1d2c1f1047fc8497433d37905b2
dmytromihalevich <mihalevicmaria502@gmail.com>
2025-08-12 20:35:32 +0300

    first version

--- forum_app/models.py
+++ forum_app/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
 
+class Topic(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=250)  
-    description = models.TextField(blank=True)  
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,5 +23,4 @@ class Post(models.Model):
             raise ValidationError("Повідомлення має бути не менше 5 символів")
 
     def __str__(self):
-        return self.title
-
+        return f"{self.author.username}: {self.content[:30]}"
