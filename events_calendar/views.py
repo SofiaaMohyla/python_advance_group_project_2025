015e0d1b7351be36667187a6a6da6c141a1c7cfb
BossDan9I <danylo.logika@gmail.com>
2025-08-26 20:41:22 +0300

    Finish calendar first part

--- events_calendar/views.py
+++ events_calendar/views.py
@@ -2,19 +2,20 @@ from django.http import HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from events_calendar.forms import CalendarForm
 from events_calendar.models import Calendar
 # Create your views here.
-class CalendarListView(ListView):
+class CalendarListView(ListView, LoginRequiredMixin):
     model = Calendar
     template_name = 'events_calendar/calendar_list.html'
     context_object_name = 'calendars'
-class CalendarDetailView(DetailView):
+class CalendarDetailView(DetailView, LoginRequiredMixin):
     model = Calendar
     template_name = 'events_calendar/calendar_detail.html'
     context_object_name = 'calendar'
-class CalendarCreateView(CreateView):
+class CalendarCreateView(CreateView, LoginRequiredMixin):
     model = Calendar
     template_name = 'events_calendar/calendar_create.html'
     form_class = CalendarForm
@@ -22,17 +23,18 @@ class CalendarCreateView(CreateView):
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
-class CalendarUpdateView(UpdateView):
+class CalendarUpdateView(UpdateView, LoginRequiredMixin):
     model = Calendar
     template_name = 'events_calendar/calendar_form.html'
-    fields = ['name']
+    # fields = ['name']
+    form_class = CalendarForm
     success_url = '/calendars/'
     def dispatch(self, request, *args, **kwargs):
         self.object = self.get_object()
-        if self.object.created_by != request.user:
-            return HttpResponseForbidden("Ви не можете редагувати це завдання")
+        # if self.object.created_by != request.user:
+        #     return HttpResponseForbidden("Ви не можете редагувати це завдання")
         return super().dispatch(request, *args, **kwargs)
-class CalendarDeleteView(DeleteView):
+class CalendarDeleteView(DeleteView, LoginRequiredMixin):
     model = Calendar
     template_name = 'events_calendar/calendar_confirm_delete.html'
     success_url = '/calendars/'
