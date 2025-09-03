310b7e2dc275da0b9f9c11031a69db1a149946ad
BossDan9I <danylo.logika@gmail.com>
2025-09-03 23:40:27 +0300

    Last fix

--- events_calendar/views.py
+++ events_calendar/views.py
@@ -7,11 +7,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from events_calendar.forms import CalendarForm, EventForm
 from events_calendar.models import Calendar, Event
 # Create your views here.
-class CalendarListView(ListView, LoginRequiredMixin):
+class CalendarListView(LoginRequiredMixin, ListView):
     model = Calendar
     template_name = 'events_calendar/calendar_list.html'
     context_object_name = 'calendars'
-class CalendarCreateView(CreateView, LoginRequiredMixin):
+class CalendarCreateView(LoginRequiredMixin, CreateView):
     model = Calendar
     template_name = 'events_calendar/calendar_create.html'
     form_class = CalendarForm
@@ -19,7 +19,7 @@ class CalendarCreateView(CreateView, LoginRequiredMixin):
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
-class CalendarUpdateView(UpdateView, LoginRequiredMixin):
+class CalendarUpdateView(LoginRequiredMixin, UpdateView):
     model = Calendar
     template_name = 'events_calendar/calendar_form.html'
     # fields = ['name']
@@ -30,13 +30,13 @@ class CalendarUpdateView(UpdateView, LoginRequiredMixin):
         # if self.object.created_by != request.user:
         #     return HttpResponseForbidden("Ви не можете редагувати це завдання")
         return super().dispatch(request, *args, **kwargs)
-class CalendarDeleteView(DeleteView, LoginRequiredMixin):
+class CalendarDeleteView(LoginRequiredMixin, DeleteView):
     model = Calendar
     template_name = 'events_calendar/calendar_confirm_delete.html'
     success_url = '/calendars/'
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
-class CalendarDetailView(DetailView, LoginRequiredMixin):
+class CalendarDetailView(LoginRequiredMixin, DetailView):
     model = Calendar
     template_name = 'events_calendar/calendar_detail.html'
     context_object_name = 'calendar'
@@ -45,7 +45,7 @@ class CalendarDetailView(DetailView, LoginRequiredMixin):
         # Отримати всі події, пов'язані з цим календарем
         context['events'] = self.object.events.all()
         return context
-class EventCreateView(CreateView, LoginRequiredMixin):
+class EventCreateView(LoginRequiredMixin, CreateView):
     model = Event
     template_name = 'events_calendar/event_create.html'
     form_class = EventForm
@@ -74,6 +74,6 @@ class EventUpdateView(LoginRequiredMixin, UpdateView):
 
 
 
-class EventDetailView(DetailView, LoginRequiredMixin):
+class EventDetailView(LoginRequiredMixin, DetailView):
     model = Event
     template_name="events_calendar/event_detail.html"
