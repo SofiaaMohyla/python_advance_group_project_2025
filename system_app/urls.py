4e94c633a5ccd29d472c8a3039061a41afe6ed18
volodymyrlogika <volodymyr.logika@gmail.com>
2025-09-09 19:48:49 +0300

    Update URL pattern for TestListView to use root path and add migration to order events by date

--- system_app/urls.py
+++ system_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.TestListView.as_view(), name='test_list'),
+    path('', views.TestListView.as_view(), name='test_list'),
     path('test/<int:pk>/question/<int:question_id>/', views.take_test, name='test/take_test'),
     path('test/<int:pk>/result/', views.TestResultView.as_view(), name='test/test_result'),
     path('test/create/', views.TestCreateView.as_view(), name='test/test_create'),
