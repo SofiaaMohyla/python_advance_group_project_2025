0982ee64cf17de54e3105eefb532a34460bb7373
NazarSadovij <sadovijnazar52@gmail.com>
2025-10-14 19:41:23 +0300

    .

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,20 +1,16 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Advertisement
-from .forms import AdvertisementForm
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class AdvertisementListView(ListView):
     model = Advertisement
     template_name = 'advertisement/advertisement_list.html'
     context_object_name = 'advertisements'
-    ordering = ['-created_at']
-
 
 class AdvertisementCreateView(LoginRequiredMixin, CreateView):
     model = Advertisement
-    form_class = AdvertisementForm
+    fields = ['title', 'description']
     template_name = 'advertisement/advertisement_form.html'
     success_url = reverse_lazy('advertisement:list')
 
@@ -22,17 +18,14 @@ class AdvertisementCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-
 class AdvertisementUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Advertisement
-    form_class = AdvertisementForm
+    fields = ['title', 'description']
     template_name = 'advertisement/advertisement_form.html'
     success_url = reverse_lazy('advertisement:list')
 
     def test_func(self):
-        advertisement = self.get_object()
-        return advertisement.author == self.request.user
-
+        return self.get_object().author == self.request.user
 
 class AdvertisementDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Advertisement
@@ -40,5 +33,4 @@ class AdvertisementDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteVie
     success_url = reverse_lazy('advertisement:list')
 
     def test_func(self):
-        advertisement = self.get_object()
-        return advertisement.author == self.request.user
+        return self.get_object().author == self.request.user
