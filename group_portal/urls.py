f2d239c1f15f714afb8948539fa0463633d750ef
volodymyrlogika <volodymyr.logika@gmail.com>
2025-11-19 13:02:48 +0200

    .

--- group_portal/urls.py
+++ group_portal/urls.py
@@ -30,7 +30,11 @@ urlpatterns = [
     path('portfolio/', include("portfolio.urls")),
     path('forum/', include("forum_app.urls")),
     path('', include("home.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Роздача media файлів (працює і на development, і на production)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     
 
